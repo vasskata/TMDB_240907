b226dbc75562c94464c3ee4d8a9616c6afc2a48a
Vincze-Vass Kata <007katy@gmail.com>
2024-09-22 17:30:07 +0200

    movie_list model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 import os, sys
 from py_components.resources import Resources
+from py_components.movie_list import MovieList
 
 APP_ROOT = os.path.dirname(__file__)
 MAIN_QML = os.path.join(APP_ROOT, "main.qml")
@@ -15,8 +16,6 @@ class TMDB:
         # Instance of QQmlApplicationEngine
         self.engine = QQmlApplicationEngine()
         
-        # Load main.qml
-        self.engine.load(MAIN_QML)
 
         # Get root context from engine
         self.context = self.engine.rootContext()
@@ -25,6 +24,12 @@ class TMDB:
         self.resources = Resources()
         self.context.setContextProperty("Resources", self.resources)
 
+        self.movie_list = MovieList()
+        self.context.setContextProperty("MovieList", self.movie_list)
+
+        # Load main.qml
+        self.engine.load(MAIN_QML)
+        
         # Check if qml has root objects
         if not self.engine.rootObjects():
             sys.exit(-1)
